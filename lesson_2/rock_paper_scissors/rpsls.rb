6f3699aaf331c9d0a45d324bcef8a633053da7fc
Fred Durham <f.durham@protonmail.com>
2021-06-11 07:26:28 -0500

    finalized code for review

--- lesson_2/rock_paper_scissors/rpsls.rb
+++ lesson_2/rock_paper_scissors/rpsls.rb
@@ -1,91 +1,91 @@
 # rpsls.rb
 # Rock, Paper, Scissors, Lizard, Spock
-# A take on the classic Rock, Paper, Scissors game that adds 
-# 2 new elements to it: Lizard and Spock. 
-# From the tv show "The Big Bang Theory"
-
-# Intro
-# Explain Rules
-# Gameplay
-# - Code rules for winning and losing (hash maybe?)
-# - best 2 out of three (need to keep track of 2 players score)
-# -display the pc/computer choices, win/lose results, score tally, 
-#   finaly winner tally
-# -play again?
-# random ideas: maybe have menu with options for moves to choose from, 
-#    Score Count, rules, etc
+# A take on the classic Rock, Paper, Scissors game that adds
+# 2 new elements to it: Lizard and Spock.
+# Invented by Sam Kass and Karen Bryla
 
 require 'yaml'
 require 'colorize'
 
 DISPLAY_MESSAGES = YAML.load_file('rpsls.yml')
-VALID_CHOICES = %w(rock paper scissors lizard spock r p sc l sp)
+VALID_CHOICES = %w[rock paper scissors lizard spock r p sc l sp]
 WINS_AGAINST = {
-  'rock'     => %w(scissors lizard),
-  'paper'    => %w(rock spock),
-  'scissors' => %w(lizard paper),
-  'lizard'   => %w(paper spock),
-  'spock'    => %w(rock scissors)
+  'rock'     => %w[scissors lizard],
+  'paper'    => %w[rock spock],
+  'scissors' => %w[lizard paper],
+  'lizard'   => %w[paper spock],
+  'spock'    => %w[rock scissors]
 }
 ABBREVIATIONS = {
-  'r' => 'rock',
-  'p' => 'paper',
+  'r'  => 'rock',
+  'p'  => 'paper',
   'sc' => 'scissors',
-  'l' => 'lizard',
+  'l'  => 'lizard',
   'sp' => 'spock'
 }
 
 ATTACKS = {
-  'rock'     => { 'lizard' => 'CRUSHES'.colorize(:light_yellow),
-                  'scissors' => 'OBLITERATES'.colorize(:magenta)},
-  'paper'    => { 'rock' => 'COVERS'.colorize(:light_black),
-                  'spock' => 'DISPROVES'.colorize(:light_magenta)},
-  'scissors' => { 'paper' => 'CUTS'.colorize(:light_red),
-                  'lizard' => 'DECAPITATES'.colorize(:red)},
-  'lizard'   => { 'spock' => 'POISONS'.colorize(:green),
-                  'paper' => 'EATS'.colorize(:light_white)},
-  'spock'    => { 'rock'=> 'VAPORIZES'.colorize(:cyan),
-                  'scissors' => 'SMASHES'.colorize(:yellow)}
+  'rock'     => { 'lizard'   => 'CRUSHES'.colorize(:light_yellow),
+                  'scissors' => 'OBLITERATES'.colorize(:magenta) },
+  'paper'    => { 'rock'     => 'COVERS'.colorize(:light_black),
+                  'spock'    => 'DISPROVES'.colorize(:light_magenta) },
+  'scissors' => { 'paper'    => 'CUTS'.colorize(:light_red),
+                  'lizard'   => 'DECAPITATES'.colorize(:red) },
+  'lizard'   => { 'spock'    => 'POISONS'.colorize(:green),
+                  'paper'    => 'EATS'.colorize(:light_white) },
+  'spock'    => { 'rock'     => 'VAPORIZES'.colorize(:cyan),
+                  'scissors' => 'SMASHES'.colorize(:yellow) }
 }
+
+COLORS = [
+  :light_black, :red, :light_red, :green, :light_green, :yellow, :light_yellow,
+  :light_blue, :magenta, :light_magenta, :cyan, :light_cyan, :default
+]
 
 # -------------------- METHOD DEFINITIONS -----------------------
 
-def add_win_to_score(score)
-  score += 1
-end
-
 def clear_screen
   system('clear')
+end
+
+def calculate_round_winner(player_name, player_weapon, jerrybot_weapon)
+  if player_weapon == jerrybot_weapon
+    'tie'
+  elsif WINS_AGAINST[player_weapon].include?(jerrybot_weapon)
+    [player_name, player_weapon, 'JerryBot', jerrybot_weapon]
+  else
+    ['JerryBot', jerrybot_weapon, player_name, player_weapon]
+  end
 end
 
 def convert_abbreviation(abbreviation)
   ABBREVIATIONS[abbreviation]
 end
 
-def calculate_round_winner(player_name, player_choice, computer_choice) 
-  if player_choice == computer_choice
-    'draw'
-  elsif WINS_AGAINST[player_choice].include?(computer_choice)
-    [player_name, player_choice, 'Jerry', computer_choice]
-  else
-    ['Jerry', computer_choice, player_name, player_choice]
-  end
-end
-
 def determine_attack_style(winners_weapon, losers_weapon)
   ATTACKS[winners_weapon][losers_weapon]
 end
 
-def display_intro_text
-  puts "Welcome to Rock Paper Scissors Lizard Spock!"
-  print DISPLAY_MESSAGES['intro_text']
-end
+def display_grand_champion(grand_champion, jerrybot_color, player_name)
+  grand_champion = jerrybot_color if grand_champion == 'JerryBot'
 
-def display_intro_graphic
-  print DISPLAY_MESSAGES['intro_graphic'].colorize(:cyan)
+  gc_color_string = ''
+  'GRAND CHAMPION!!'.each_char do |letter|
+    gc_color_string << letter.colorize(COLORS.sample)
+  end
+
+  puts "It looks like #{grand_champion} is our #{gc_color_string}"
+  if grand_champion == jerrybot_color
+    puts "You win some, you lose some. Better luck next time #{player_name}."
+    puts
+  end
 end
 
 def display_choices_prompt
+  puts
+  puts "Which weapon do you choose?"
+  puts "Abbreviations are ok."
+  puts
   print "[(R)ock (P)aper (Sc)issors (L)izard (Sp)ock]=> "
 end
 
@@ -93,55 +93,78 @@ def display_input_prompt
   print "--> "
 end
 
-def display_message(message)
+def display_intro_graphic
+  print DISPLAY_MESSAGES['intro_graphic'].colorize(:cyan)
+end
+
+def display_intro_text
+  puts "Welcome to Rock Paper Scissors Lizard Spock!"
+  print DISPLAY_MESSAGES['intro_text']
 end
 
 def display_outro
   puts DISPLAY_MESSAGES['outro'].colorize(:yellow)
 end
 
-def display_results(player_name, player_weapon, computer_weapon, round_winner, 
-                    winning_weapon, round_loser, losing_weapon)
+def display_round_results(round_winner, winning_weapon, round_loser,
+                          losing_weapon, jerrybot_color)
+
+  round_winner = jerrybot_color if round_winner == 'JerryBot'
+
+  puts "#{round_winner}'s #{winning_weapon.capitalize} "\
+       "#{determine_attack_style(winning_weapon, losing_weapon)} "\
+       "#{round_loser}'s #{losing_weapon.capitalize}!!!"
+  puts
+  puts "#{round_winner} wins this round!"
+  puts
+end
+
+def display_score(score, player)
+  puts "The score so far is:"
+  puts "#{player}: #{score[player]}  #{'JerryBot'.colorize(:light_red)}: "\
+       "#{score['JerryBot']}  Ties: #{score['tie']}"
+  puts "---------------------------------------------------------------------"
+  puts
+end
+
+def display_tie
+  puts
+  puts "It's a tie! Noone gets a point this round."
+  puts
+end
+
+def display_weapon_choices(player_name, player_weapon, jerrybot_color,
+                           jerrybot_weapon)
   puts
   puts "#{player_name} chooses: #{player_weapon.capitalize}!"
-  puts "Jerry chooses: #{computer_weapon.capitalize}!"
-  if round_winner == 'draw'
-    puts "It's a draw!! Nobody wins.".colorize(:cyan)
-  else
-    puts "#{round_winner}'s #{winning_weapon.capitalize} "\
-         "#{determine_attack_style(winning_weapon, losing_weapon)} "\
-         "#{round_loser}'s #{losing_weapon.capitalize}!!!"
-    puts
-    puts "#{round_winner} wins this round!"
-    puts
-  end
+  puts "#{jerrybot_color} chooses: #{jerrybot_weapon.capitalize}!"
+  puts
 end
 
-def get_computer_choice
-  %w(rock paper scissors lizard spock).sample
-end
-
-def get_player_choice
-  puts "Which weapon do you choose? Choose wisely..."
-
-  player_choice = ''
-  loop do
-    display_choices_prompt()
-    player_choice = gets.chomp.downcase.strip
-    break if valid_input?(player_choice,VALID_CHOICES)
-    puts "That's not a valid choice. Choose again."
-  end
-  
-  player_choice = 
-    convert_abbreviation(player_choice) if ABBREVIATIONS.include?(player_choice)
-  player_choice
+def get_jerrybot_weapon_choice
+  %w[rock paper scissors lizard spock].sample
 end
 
 def get_player_name
   print "What name do you go by? "
   display_input_prompt()
   name = gets.chomp.strip
-  name
+  name.colorize(:cyan)
+end
+
+def get_player_weapon_choice
+  player_choice = ''
+  loop do
+    display_choices_prompt()
+    player_choice = gets.chomp.downcase.strip
+    break if valid_input?(player_choice, VALID_CHOICES)
+    puts "That's not a valid choice. Choose again."
+  end
+
+  if ABBREVIATIONS.include?(player_choice)
+    player_choice = convert_abbreviation(player_choice)
+  end
+  player_choice
 end
 
 def play_again?
@@ -152,7 +175,7 @@ def play_again?
   loop do
     display_input_prompt()
     user_choice = gets.chomp.downcase.strip
-    break if valid_input?(user_choice,'y', 'n')
+    break if valid_input?(user_choice, 'y', 'n')
     puts "You have to enter either either 'y' or 'n'. Try again."
   end
 
@@ -168,28 +191,47 @@ end
 
 first_run = true
 player_name = ''
+jerrybot_color = 'JerryBot'.colorize(:light_red)
+
 loop do
   clear_screen()
   display_intro_graphic()
-  display_intro_text() if first_run
+
   if first_run
+    display_intro_text()
     player_name = get_player_name()
+    puts
     puts "Hello, #{player_name} =-)"
     puts
   end
-  player_score = 0
-  computer_score = 0
-  player_choice = get_player_choice()
-  computer_choice = get_computer_choice()
-  round_winner, winning_weapon, round_loser, losing_weapon = 
-    calculate_round_winner(player_name, player_choice, computer_choice)
-  
-  display_results(player_name, player_choice, computer_choice, round_winner,
-                  winning_weapon, round_loser, losing_weapon)
+
+  score = { player_name => 0, 'JerryBot' => 0, 'tie' => 0 }
+  until score[player_name] == 3 || score['JerryBot'] == 3
+    player_weapon = get_player_weapon_choice()
+    jerrybot_weapon = get_jerrybot_weapon_choice()
+
+    round_winner, winning_weapon, round_loser, losing_weapon =
+      calculate_round_winner(player_name, player_weapon, jerrybot_weapon)
+
+    score[round_winner] += 1
+    display_weapon_choices(player_name, player_weapon, jerrybot_color,
+                           jerrybot_weapon)
+
+    if round_winner == 'tie'
+      display_tie()
+    else
+      display_round_results(round_winner, winning_weapon, round_loser,
+                            losing_weapon, jerrybot_color)
+    end
+    display_score(score, player_name)
+
+  end
+
+  grand_champion = score.key(3)
+  display_grand_champion(grand_champion, jerrybot_color, player_name)
+
   first_run = false
   break unless play_again?()
-
 end
 
 display_outro()
-
